627a864e64a8d28ffea9845c8771c6d7b5d78627
adityagadre <adivb2003@gmail.com>
2014-01-26 02:01:49 -0800

    GetUser

--- PinHealthTest/src/src/main/java/com/fitbit/web/FitbitDBUtil.java
+++ PinHealthTest/src/src/main/java/com/fitbit/web/FitbitDBUtil.java
@@ -2,7 +2,11 @@ package src.main.java.com.fitbit.web;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Properties;
 
 import com.fitbit.api.common.model.user.UserInfo;
 import com.mysql.jdbc.PreparedStatement;
@@ -33,11 +37,38 @@ public class FitbitDBUtil {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
-		
-		
 		// TODO Auto-generated method stub
+	}
+	
+	public static Properties getUser(int id)
+	{
+		Properties p=new Properties();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection connect = DriverManager.getConnection("jdbc:mysql://54.193.91.8/market","abc","a");
+
+			Statement s=(Statement) connect.createStatement();
+			
+			ResultSet rs=s.executeQuery("select * from patient where id=" + id);
+			
+			rs.first();
+			p.setProperty("auth_token", rs.getString(2));
+			p.setProperty("auth_verifier", rs.getString(3));
+			rs.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return p;
+		
 		
 	}
 
